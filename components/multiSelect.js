1188209ef3999b082e4b78278c9056bc925c3154
Maxime St-Louis-Fortier <stlouisfortiermaxime@gmail.com>
2021-03-07 07:43:58 -0500

    Adds a search for the multi-select component

--- components/multiSelect.js
+++ components/multiSelect.js
@@ -2,15 +2,28 @@ Vue.component('multi-select', {
     props: ['options', 'name'],
     data: function(){
         return {
-            isHidden: true
+            isHidden: true,
+            search: "",
         }
     },
     methods:{
-        toggleHidden: function(event){
+        toggleHidden: function(){
             this.isHidden = !this.isHidden;
             if(this.isHidden === true){
                 this.$emit('closed');
+            }else{
+                this.$nextTick(() =>
+                    this.$refs.searchInput.focus()
+                );
             }
+        },
+        switchFiltered: function(switchTo){
+            this.filteredOptions.forEach((el) => {
+                if (el.selected === !switchTo) {
+                    this.$emit('selected', el.index);
+                }
+            })
+
         },
         unselectAll: function(){
             let changed = false;
@@ -20,10 +33,36 @@ Vue.component('multi-select', {
                     changed = true;
                 }
             })
-            this.$emit('closed');
+            if(changed === true){
+                this.$emit('closed');
+            }
+        },
+        toggleOption: function(index){
+            this.$emit('selected', index)
+        },
+        refocus: function(){
+            if (this.isHidden === false){
+                this.$refs.searchInput.focus();
+            }
+        },
+        preventDefault: function(event){
+            event.stopImmediatePropagation();
         }
     },
     computed:{
+        filteredOptions: function(){
+            let opt = this.options.map((el, index) => {
+                el.index = index;
+                return el;
+            })
+
+            if(this.search === "")
+                return opt;
+            
+            return opt.filter((el) =>{ 
+                return el.displayName.toLowerCase().includes(this.search); 
+            });
+        },
         selectedOptions: function(){
             return this.options.filter((el) => { return el.selected === true;} ).map((el) => { return el.displayName}).sort();
         },
@@ -33,20 +72,33 @@ Vue.component('multi-select', {
     },
     template: `
     <div class="multi-select">
-        <div>
-            <div class="multi-select-input form-control" @click.self="toggleHidden($event)">
+        <div @click="refocus()">
+            <div v-if="isHidden" class="multi-select-input form-control" @click.self="toggleHidden()">
                 <template v-if="selectedOptions.length > 0">
                     ({{selectedOptions.length}}) {{selectedOptions.join(', ')}}
                     <div class="multi-select-close" @click="unselectAll"><i class="bi bi-x"></i></div>
                 </template>
                 <template v-else>All</template>
-                
             </div>
-            <div class="multi-select-options" :class="{hidden: isHidden}">
-                <div v-for="(option, index) in options" class="form-check form-switch multi-select-option">
-                    <input class="form-check-input" type="checkbox" :id="id + '-' + index" :checked="option.selected" @change="$emit('selected', index)"/>
-                    <label class="form-check-label" :for="id + '-' + index">{{option.displayName}}</label>
-                </div>
+            <div v-else class="input-group">
+                <input class="multi-select-input form-control" v-model="search" ref="searchInput" @keydown.esc="toggleHidden()"/>
+                <button class="btn btn-primary" type="button" @click="toggleHidden()"><i class="bi bi-check2-circle"></i></button>
+            </div>
+            
+            <div class="multi-select-option-list" :class="{hidden: isHidden}">
+                <template v-if="filteredOptions.length > 0">
+                    <div class="input-group w-100 pb-2">
+                        <button class="btn btn-dark w-50" @click="switchFiltered(true)">Select</button>
+                        <button class="btn btn-dark w-50" @click="switchFiltered(false)">Clear</button>
+                    </div>
+                    <div class="options">
+                        <div v-for="option in filteredOptions" class="form-check form-switch multi-select-option">
+                            <input class="form-check-input" type="checkbox" :id="id + '-' + option.index" :checked="option.selected" @change="toggleOption(option.index)"/>
+                            <label class="form-check-label" :for="id + '-' + option.index">{{option.displayName}}</label>
+                        </div>
+                    </div>
+                </template>
+                <div v-else style="text-align: center">Nothing matches your search</div>
             </div>
         </div>
     </div>
