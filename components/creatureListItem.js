60438b53be9e14a70f03f19dc56145be7b5bb2f5
Revictas <marcouiller.stephane05@gmail.com>
2021-10-17 01:45:11 -0400

    couple improvements + updates

--- components/creatureListItem.js
+++ components/creatureListItem.js
@@ -9,12 +9,36 @@ Vue.component('creature-list-item', {
                 <button type="button" class="btn btn-success status-button" v-on:click="$emit('add-elite', creature)"><i class="bi bi-shield-plus"></i></button>
             </div >
         </td >
-        <td><a :href="creature.link" @click.prevent="$emit('open-link', creature.link)">{{ creature.name }}</a></td >
-        <td>{{ creature.level }}</td>
-        <td>{{ creature.family }}</td>
-        <td>{{ creature.alignment }}</td>
-        <td>{{ creature.type }}</td>
-        <td>{{ creature.size }}</td>
+        <td><a :href="creature.name.link" @click.prevent.exact="$emit('open-link', creature.name.link)" v-on:click.ctrl.exact="$emit('add-filter', [creature.name.name, 'name'])">{{ creature.name.name }}</a></td >
+        <td><span v-on:click.ctrl.exact="$emit('add-filter', [creature.level, 'level'])">{{ creature.level }}</span></td>
+        <td>
+            <a v-if="creature.family.link !== ''" :href="creature.family.link" @click.prevent.exact="$emit('open-link', creature.family.link)" v-on:click.ctrl.exact="$emit('add-filter', [creature.family.name, 'family'])">{{ creature.family.name }}</a>
+            <template v-else>
+                {{creature.family.name}}
+            </template>
+        </td>
+        <td><span v-on:click.ctrl.exact="$emit('add-filter', [creature.alignment, 'alignment'])">{{ creature.alignment }}</span></td>
+        <td>
+            <span v-for="(type, index) in creature.types">  
+                <a v-if="type.link !== ''" :href="type.link" @click.prevent.exact="$emit('open-link', type.link)" v-on:click.ctrl.exact="$emit('add-filter', [type.name, 'type'])">{{ type.name }}</a>
+                <template v-else>
+                    {{type.name}}
+                </template>
+                <span v-if="index < creature.types.length -1"></span>
+            </span>
+            {{ creature.type }}
+        </td>
+        <td><span v-on:click.ctrl.exact="$emit('add-filter', [creature.size, 'size'])">{{ creature.size }}</span></td>
+        <td>
+            <span v-for="(trait, index) in creature.traits">  
+                <a v-if="trait.link !== ''" :href="trait.link" @click.prevent.exact="$emit('open-link', trait.link)" v-on:click.ctrl.exact="$emit('add-filter', [trait.name, 'trait'])">{{ trait.name }}</a>
+                <template v-else>
+                    {{trait.name}}
+                </template>
+                <span v-if="index < creature.traits.length -1"></span>
+            </span>
+            {{ creature.type }}
+        </td>
     </tr>
     `
-})
+});
